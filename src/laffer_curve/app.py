f4d35b6d8fc6c7c0d12a23e8f36a3d3e9e4f5828
Juliette.Bornier <juliette.bornier.3110@gmail.com>
2024-07-11 16:20:12 +0200

    create a folder for styles

--- src/laffer_curve/app.py
+++ src/laffer_curve/app.py
@@ -30,7 +30,7 @@ def main():
         with open(file_name) as f:
             st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-    local_css(package_dir / "styles.css")
+    local_css(package_dir / "assets/styles.css")
 
     st.markdown(
         """
